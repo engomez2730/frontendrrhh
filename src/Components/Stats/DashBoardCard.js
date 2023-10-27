73dd9b3082e57699907bbce5b890f13248b517ab
Your Name <enderson273014@gmail.com>
2023-10-27 16:59:52 -0400

    Api

--- src/Components/Stats/DashBoardCard.js
+++ src/Components/Stats/DashBoardCard.js
@@ -5,7 +5,12 @@ const DashboardCard = ({ title, value, icon }) => {
     <Card style={{ width: "20%", marginLeft: "40px" }}>
       <Space direction="horizontal">
         {icon}
-        <Statistic title={title} value={value} icon={icon} />
+        <Statistic
+          title={title}
+          value={value}
+          icon={icon}
+          loading={!value ? true : false}
+        />
       </Space>
     </Card>
   );
